b8fa23f96477caed022344baffbb8406c7d17560
Adam Elkhanoufi <aelkha5@lsu.edu>
2022-05-24 18:43:05 -0500

    v1.0

---
+++ index.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+const app = express();
+const PORT = 2000;
+
+app.use(express.json());
+
+app.listen(
+    PORT
+)
+
+app.get('/pokemon', (req, res) => {
+    res.json({
+        pokemon: 'Pikachu',
+        type: 'electric',
+        img: 'e_pikachu.png'
+    });
+})
